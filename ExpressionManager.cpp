3ee1457bb78a4f330582ae49c4fb75310d905b2b
brad <brad.spendlove@.com>
2020-05-05 17:10:34 -0600

    adding stubs

---
+++ ExpressionManager.cpp
@@ -0,0 +1,21 @@
+#include "ExpressionManager.h"
+
+bool ExpressionManager::isBalanced(string expression){
+	cout << "isbalanced: " << expression << endl;
+	return true;
+}
+
+string ExpressionManager::postfixToInfix(string postfixExpression){
+	cout << "postfixtoinfix: " << postfixExpression << endl;
+	return "REPLACE";
+}
+
+string ExpressionManager::postfixEvaluate(string postfixExpression){
+	cout << "postfixeval: " << postfixExpression << endl;
+	return "REPLACE";
+}
+
+string ExpressionManager::infixToPostfix(string infixExpression){
+	cout << "infixtopostfix: " << infixExpression << endl;
+	return "REPLACE";
+}
