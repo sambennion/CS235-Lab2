1c5edebf2a6d8e80ef47cafc7f42768410d48c87
Brad Spendlove <brad.spendlove@gmail.com>
2020-05-05 17:11:33 -0600

    Update ExpressionManager.cpp

--- ExpressionManager.cpp
+++ ExpressionManager.cpp
@@ -1,21 +1,21 @@
 #include "ExpressionManager.h"
 
 bool ExpressionManager::isBalanced(string expression){
-	cout << "isbalanced: " << expression << endl;
-	return true;
+	cout << "isBalanced: " << expression << endl;
+	return true; //REPLACE
 }
 
 string ExpressionManager::postfixToInfix(string postfixExpression){
-	cout << "postfixtoinfix: " << postfixExpression << endl;
+	cout << "postfixToInfix: " << postfixExpression << endl;
 	return "REPLACE";
 }
 
 string ExpressionManager::postfixEvaluate(string postfixExpression){
-	cout << "postfixeval: " << postfixExpression << endl;
+	cout << "postfixEvaluate: " << postfixExpression << endl;
 	return "REPLACE";
 }
 
 string ExpressionManager::infixToPostfix(string infixExpression){
-	cout << "infixtopostfix: " << infixExpression << endl;
+	cout << "infixToPostfix: " << infixExpression << endl;
 	return "REPLACE";
 }
